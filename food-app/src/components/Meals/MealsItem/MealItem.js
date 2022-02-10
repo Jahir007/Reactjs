e56e5f6b4bd1ced98a964d481d50d4116db0b7db
Jahir007 <jk584853@gmail.com>
2022-02-10 10:21:09 +0530

    master

--- food-app/src/components/Meals/MealsItem/MealItem.js
+++ food-app/src/components/Meals/MealsItem/MealItem.js
@@ -1,4 +1,5 @@
 import classes from './MealItem.module.css'
+import MealItemForm from './MealItemForm';
 
 const MealItem = (props) => {
     
@@ -13,7 +14,7 @@ const MealItem = (props) => {
                     <div className={classes.price}>{props.price}</div>
                 </div>
                 <div>
-
+                    <MealItemForm />
                 </div>    
             </li>
         </>
