3f8d52fa27f3cc5c0f9fb7b9e0a078c3ce32fb2e
Jahir007 <jk584853@gmail.com>
2022-02-25 11:35:30 +0530

    master

--- api/src/App.js
+++ api/src/App.js
@@ -1,40 +1,21 @@
-import React, { useState } from 'react';
-
-import RecordList from './components/RecordList';
-import './App.css';
+import "./App.css";
+import useFetch from "./useFetch";
 
 function App() {
-  const [records, setRecords] = useState([]);
+  const { data, loading, error } = useFetch(
+    "http://demos1.stral.in/PGCTechnologyServices/api/v1/course/catalog?_format=json"
+  );
 
-  function fetchRecordHandler() {
-    fetch('http://demos1.stral.in/PGCTechnologyServices/api/v1/course/catalog?_format=json')
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-       // var recordData=data;
-       // console.log(data.Results)
-        const transformedRecords = data.Results.map((recordData) => {
-          return {
-            id: recordData.CourseID,
-            title: recordData.AvailableSeats,
-            // openingText: movieData.opening_crawl,
-            releaseDate: recordData.RegistrationDeadline,
-          };
-        });
-        setRecords(transformedRecords);
-      });
-  }
+  if (loading) return <h1> LOADING...</h1>;
+
+  if (error) console.log(error);
 
   return (
-    <React.Fragment>
-      <section>
-        <button onClick={fetchRecordHandler}>Fetch Details</button>
-      </section>
-      <section>
-        <RecordList records={records} />
-      </section>
-    </React.Fragment>
+    
+    <div className="App">
+      <h1>{data?.CourseID}</h1>
+    </div>
+    
   );
 }
 
