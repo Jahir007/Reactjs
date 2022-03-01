208eb488ccb34c90206134edb4035b9aa5c95ebc
Jahir007 <jk584853@gmail.com>
2022-03-01 10:25:56 +0530

    master

--- reactadduser/src/App.js
+++ reactadduser/src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//Import react routes and its other modules
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+
+//All components
+import AddUser from './AddUser';
+import Home from './Home';
+import Header from './Header';
+
+//bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+//Impoert axios services
+import axios from 'axios';
+
+
+
+class App extends React.Component {
+  
+ 
+  render() {
+   
+    return (
+      <Router>
+      <div className="maincontainer">
+        <Header></Header>
+        
+
+        <Switch>
+            
+            
+            <Route exact path='/adduser' component={AddUser} />
+            <Route exact path='/home' component={Home} />
+            <Route exact path='' component={Home} />
+           
+          </Switch>
+            
+      
+
+      
+      </div>
+      </Router>
+)
+};
 }
 
 export default App;
