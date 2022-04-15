c222e6104c4d819a6404c2c8c410cc6466cd4670
Jahir007 <jk584853@gmail.com>
2022-04-15 10:17:20 +0530

    master

--- parksl/park/src/assets/admin/js/scripts.js
+++ parksl/park/src/assets/admin/js/scripts.js
@@ -7,20 +7,41 @@
 // Scripts
 // 
 
-window.addEventListener('DOMContentLoaded', event => {
+// window.addEventListener('DOMContentLoaded', event => {
+
+//     // Toggle the side navigation
+//     const sidebarToggle = document.body.querySelector('#sidebarToggle');
+//     if (sidebarToggle) {
+//         // Uncomment Below to persist sidebar toggle between refreshes
+//         // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
+//         //     document.body.classList.toggle('sb-sidenav-toggled');
+//         // }
+//         sidebarToggle.addEventListener('click', event => {
+//             event.preventDefault();
+//             document.body.classList.toggle('sb-sidenav-toggled');
+//             localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+//         });
+//     }
+
+// });
+
+import jQuery from 'jquery';
+
+(function($) {
+    
+    setTimeout(function(){
+    // Add active state to sidbar nav links
+    var path = window.location.href; // because the 'href' property of the DOM element is the absolute path
+        $("#layoutSidenav_nav .sb-sidenav a.nav-link").each(function() {
+            if (this.href === path) {
+                $(this).addClass("active");
+            }
+        });
 
     // Toggle the side navigation
-    const sidebarToggle = document.body.querySelector('#sidebarToggle');
-    if (sidebarToggle) {
-        // Uncomment Below to persist sidebar toggle between refreshes
-        // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
-        //     document.body.classList.toggle('sb-sidenav-toggled');
-        // }
-        sidebarToggle.addEventListener('click', event => {
-            event.preventDefault();
-            document.body.classList.toggle('sb-sidenav-toggled');
-            localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
-        });
-    }
-
-});
+    $("#sidebarToggle").on("click", function(e) {
+        e.preventDefault();
+        $("body").toggleClass("sb-sidenav-toggled");
+    });
+}, 3000);
+})(jQuery);
--- parksl/park/src/layouts/admin/Navbar.js
+++ parksl/park/src/layouts/admin/Navbar.js
@@ -9,8 +9,8 @@ const Navbar = () => {
         
             <Link className="navbar-brand ps-3" to="/admin">Parking Portal</Link>
             
-            <button className="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i className="fas fa-bars"></i></button>
-            
+            {/* <button className="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i className="fas fa-bars"></i></button> */}
+            <button className="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#"><i className="fas fa-bars"></i></button>
             <form className="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
 
                 <div className="input-group">
